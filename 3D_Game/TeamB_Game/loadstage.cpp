67afd136e7018571ed93c4961cb7c8fc2e8fb97a
Kensaku-Hatori <241316@st.yoshida-g.ac.jp>
2025-01-17 17:11:06 +0900

    test

---
+++ 3D_Game/TeamB_Game/loadstage.cpp
@@ -0,0 +1,12 @@
+#include "loadstage.h"
+
+void LoadModelViewer()
+{
+
+	FILE* pFile = fopen("data\\TEXT\\model.txt", "r");
+
+	while (1)
+	{
+
+	}
+}
---
+++ 3D_Game/TeamB_Game/loadstage.h
@@ -0,0 +1,13 @@
+//*************************************************
+// 
+// ƒ‚ƒfƒ‹ƒrƒ…[ƒ“Ç‚İ‚İˆ—[loadstage.h]
+// Author Kensaku Hatori
+// 
+//*************************************************
+#ifndef LOADSTAGE_H_
+#define LOADSTAGE_H_
+
+#include "main.h"
+
+void LoadModelViewer();
+#endif // !LOADSTAGE_H_
