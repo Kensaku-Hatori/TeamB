7d440c346cf9413a39e7669c27c7db171ee436c2
大野莉緒 <241302@st.yoshida-g.ac.jp>
2025-01-21 09:46:11 +0900

    ごめん

--- 3D_Game/TeamB_Game/meshfield.h
+++ 3D_Game/TeamB_Game/meshfield.h
@@ -21,6 +21,36 @@
 
 #define MESH_SIZE (150.0f)
 
+#define MESH_NUM_MAX (1)//メッシュフィールドの数
+
+
+//テクスチャタイプ
+typedef enum
+{
+	MESH_TEX_NULL=0,
+	MESH_TEX_MAX
+}MESH_TEX;
+
+//ファイル格納
+static const char* MESHFIELD_TEXTURE[MESH_TEX_MAX] =
+{
+	NULL
+};
+
+//ポリゴン(横)の構造体
+typedef struct
+{
+	D3DXVECTOR3 pos;						//位置division
+	D3DXVECTOR3 rot;						//向き
+	D3DXMATRIX mtxWorld;					//ワールドマトリックス
+	MESH_TEX textype;						//テクスチャのタイプ
+	int DiviX;								//分割数x
+	int DiviZ;								//分割数z
+	float fWidth;							//幅
+	float fHeight;							//高さ
+	bool bUse;								//使用しているかどうか
+}MeshField;
+
 //プロトタイプ宣言
 void InitMeshfield(void);
 void UninitMeshfield(void);
