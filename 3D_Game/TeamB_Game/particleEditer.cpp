da02a49ced7b5e9296b49dd1f02f5a8436081a96
Kensaku-Hatori <241316@st.yoshida-g.ac.jp>
2025-01-23 12:20:55 +0900

    error

--- 3D_Game/TeamB_Game/particleEditer.cpp
+++ 3D_Game/TeamB_Game/particleEditer.cpp
@@ -34,30 +34,30 @@ void UpdateParticleEditer()
 	UpdateCamera();
 	if (GetKeyboardPress(DIK_LSHIFT))
 	{
-		if (GetKeyboardPress(DIK_R) && g_ParticleEditer.ParticleInfo.col.r >= 0.0f)
+		if (GetKeyboardPress(DIK_R) && g_ParticleEditer.ParticleInfo.col.r > 0.0f)
 		{
 			g_ParticleEditer.ParticleInfo.col.r -= COLSPEED;
 		}
-		else if (GetKeyboardPress(DIK_G) && g_ParticleEditer.ParticleInfo.col.g <= 1.0f)
+		else if (GetKeyboardPress(DIK_G) && g_ParticleEditer.ParticleInfo.col.g > 0.0f)
 		{
 			g_ParticleEditer.ParticleInfo.col.g -= COLSPEED;
 		}
-		else if (GetKeyboardPress(DIK_B) && g_ParticleEditer.ParticleInfo.col.b <= 1.0f)
+		else if (GetKeyboardPress(DIK_B) && g_ParticleEditer.ParticleInfo.col.b > 0.0f)
 		{
 			g_ParticleEditer.ParticleInfo.col.b -= COLSPEED;
 		}
 	}
 	else
 	{
-		if (GetKeyboardPress(DIK_R) && g_ParticleEditer.ParticleInfo.col.r <= 1.0f)
+		if (GetKeyboardPress(DIK_R) && g_ParticleEditer.ParticleInfo.col.r < 1.0f)
 		{
 			g_ParticleEditer.ParticleInfo.col.r += COLSPEED;
 		}
-		else if (GetKeyboardPress(DIK_G) && g_ParticleEditer.ParticleInfo.col.g <= 1.0f)
+		else if (GetKeyboardPress(DIK_G) && g_ParticleEditer.ParticleInfo.col.g < 1.0f)
 		{
 			g_ParticleEditer.ParticleInfo.col.g += COLSPEED;
 		}
-		else if (GetKeyboardPress(DIK_B) && g_ParticleEditer.ParticleInfo.col.b <= 1.0f)
+		else if (GetKeyboardPress(DIK_B) && g_ParticleEditer.ParticleInfo.col.b < 1.0f)
 		{
 			g_ParticleEditer.ParticleInfo.col.b += COLSPEED;
 		}
