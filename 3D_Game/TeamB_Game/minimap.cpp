5a63a5f5e7e73e13c384e0e1da5d60b35d59fdf6
開地碧音 <241303@st.yoshida-g.ac.jp>
2025-01-24 11:48:51 +0900

    ミニマップ

---
+++ 3D_Game/TeamB_Game/minimap.cpp
@@ -0,0 +1,217 @@
+//==============================
+//
+//  ミニマップ処理[MiniMap.cpp]
+//  Author:kaiti
+//
+//==============================
+#include "minimap.h"
+#include "player.h"
+//グローバル変数
+//枠
+LPDIRECT3DTEXTURE9 g_pTextureMiniMap = NULL;
+LPDIRECT3DVERTEXBUFFER9 g_pVtxBuffMiniMap = NULL;
+//プレイヤーアイコン
+LPDIRECT3DTEXTURE9 g_pTextureMapPlayer = NULL;
+LPDIRECT3DVERTEXBUFFER9 g_pVtxBuffMapPlayer = NULL;
+
+D3DXVECTOR3 g_MiniMappos;
+D3DXVECTOR3 g_MapPlayerpos;
+//=============
+// 初期化処理
+//=============
+void InitMiniMap(void)
+{
+	LPDIRECT3DDEVICE9 pDevice;
+	//デバイスの取得
+	pDevice = GetDevice();
+
+	//テクスチャの読み込み
+	D3DXCreateTextureFromFile(pDevice, "data\\TEXTURE\\map.png", &g_pTextureMiniMap);	//枠
+	
+	D3DXCreateTextureFromFile(pDevice, "data\\TEXTURE\\ownmachine.jpg", &g_pTextureMapPlayer);		//HP 
+
+
+	g_MiniMappos = D3DXVECTOR3(1000.0f, 0.0f, 0.0f);
+	g_MapPlayerpos = D3DXVECTOR3(1063.0f, 140.0f, 0.0f);
+
+	//頂点バッファの生成・頂点情報の設定
+	VERTEX_2D* pVtx;
+
+	//枠
+	{		
+		//頂点バッファの生成
+		pDevice->CreateVertexBuffer(sizeof(VERTEX_2D) * 4,
+			D3DUSAGE_WRITEONLY,
+			FVF_VERTEX_2D,
+			D3DPOOL_MANAGED,
+			&g_pVtxBuffMiniMap,
+			NULL);
+		//頂点バッファをロックし、頂点情報へのポインタを取得
+		g_pVtxBuffMiniMap->Lock(0, 0, (void**)&pVtx, 0);
+
+		//頂点座標の設定
+		pVtx[0].pos = D3DXVECTOR3(g_MiniMappos.x, g_MiniMappos.y, 0.0f);
+		pVtx[1].pos = D3DXVECTOR3(g_MiniMappos.x + MINIMAP_X, g_MiniMappos.y, 0.0f);
+		pVtx[2].pos = D3DXVECTOR3(g_MiniMappos.x, g_MiniMappos.y + MINIMAP_Y, 0.0f);
+		pVtx[3].pos = D3DXVECTOR3(g_MiniMappos.x + MINIMAP_X, g_MiniMappos.y + MINIMAP_Y, 0.0f);
+		//rhwの設定
+		pVtx[0].rhw = 1.0f;
+		pVtx[1].rhw = 1.0f;
+		pVtx[2].rhw = 1.0f;
+		pVtx[3].rhw = 1.0f;
+		//頂点カラーの設定
+		pVtx[0].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+		pVtx[1].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+		pVtx[2].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+		pVtx[3].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+		//テクスチャ座標の設定
+		pVtx[0].tex = D3DXVECTOR2(0.0f, 0.0f);
+		pVtx[1].tex = D3DXVECTOR2(1.0f, 0.0f);
+		pVtx[2].tex = D3DXVECTOR2(0.0f, 1.0f);
+		pVtx[3].tex = D3DXVECTOR2(1.0f, 1.0f);
+
+		//頂点バッファをアンロック
+		g_pVtxBuffMiniMap->Unlock();
+	}
+
+	//プレイヤーアイコン
+	{
+		//頂点バッファの生成
+		pDevice->CreateVertexBuffer(sizeof(VERTEX_2D) * 4,
+			D3DUSAGE_WRITEONLY,
+			FVF_VERTEX_2D,
+			D3DPOOL_MANAGED,
+			&g_pVtxBuffMapPlayer,
+			NULL);
+		//頂点バッファをロックし、頂点情報へのポインタを取得
+		g_pVtxBuffMapPlayer->Lock(0, 0, (void**)&pVtx, 0);
+
+		//頂点座標の設定
+		pVtx[0].pos = D3DXVECTOR3(g_MapPlayerpos.x, g_MapPlayerpos.y, 0.0f);
+		pVtx[1].pos = D3DXVECTOR3(g_MapPlayerpos.x + MAPSIZE_PLAYER, g_MapPlayerpos.y, 0.0f);
+		pVtx[2].pos = D3DXVECTOR3(g_MapPlayerpos.x, g_MapPlayerpos.y + MAPSIZE_PLAYER, 0.0f);
+		pVtx[3].pos = D3DXVECTOR3(g_MapPlayerpos.x + MAPSIZE_PLAYER, g_MapPlayerpos.y + MAPSIZE_PLAYER, 0.0f);
+		//rhwの設定
+		pVtx[0].rhw = 1.0f;
+		pVtx[1].rhw = 1.0f;
+		pVtx[2].rhw = 1.0f;
+		pVtx[3].rhw = 1.0f;
+		//頂点カラーの設定
+		pVtx[0].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+		pVtx[1].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+		pVtx[2].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+		pVtx[3].col = D3DCOLOR_RGBA(255, 255, 255, 255);
+		//テクスチャ座標の設定
+		pVtx[0].tex = D3DXVECTOR2(0.0f, 0.0f);
+		pVtx[1].tex = D3DXVECTOR2(1.0f, 0.0f);
+		pVtx[2].tex = D3DXVECTOR2(0.0f, 1.0f);
+		pVtx[3].tex = D3DXVECTOR2(1.0f, 1.0f);
+
+		//頂点バッファをアンロック
+		g_pVtxBuffMapPlayer->Unlock();
+	}
+
+}
+//==========
+// 終了処理
+//==========
+void UninitMiniMap(void)
+{
+	//テクスチャの破棄
+	if (g_pTextureMiniMap != NULL && g_pTextureMapPlayer != NULL)
+	{
+		g_pTextureMiniMap->Release();
+		g_pTextureMiniMap = NULL;
+		g_pTextureMapPlayer->Release();
+		g_pTextureMapPlayer = NULL;
+
+	}
+
+	//頂点バッファの破棄
+	if (g_pVtxBuffMiniMap != NULL && g_pVtxBuffMapPlayer != NULL)
+	{
+		g_pVtxBuffMiniMap->Release();
+		g_pVtxBuffMiniMap = NULL;
+		g_pVtxBuffMapPlayer->Release();
+		g_pVtxBuffMapPlayer = NULL;
+	}
+}
+//==========
+// 更新処理
+//==========
+void UpdateMiniMap(void)
+{
+
+}
+//===========
+// 描画処理
+//===========
+void DrawMiniMap(void)
+{
+	LPDIRECT3DDEVICE9 pDevice;
+	//デバイスの取得
+	pDevice = GetDevice();
+
+	//頂点フォーマットの設定
+	pDevice->SetFVF(FVF_VERTEX_2D);
+
+	//枠
+	{
+		//頂点バッファをデータストリームに設定
+		pDevice->SetStreamSource(0, g_pVtxBuffMiniMap, 0, sizeof(VERTEX_2D));
+		//テクスチャの設定
+		pDevice->SetTexture(0, g_pTextureMiniMap);
+		//プレイヤーの描画
+		pDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 2);
+	}
+
+	//アイテム欄
+	{
+		//頂点バッファをデータストリームに設定
+		pDevice->SetStreamSource(0, g_pVtxBuffMapPlayer, 0, sizeof(VERTEX_2D));
+		//テクスチャの設定
+		pDevice->SetTexture(0, g_pTextureMapPlayer);
+		//プレイヤーの描画
+		pDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 2);
+	}
+}
+//
+//
+//
+void SetMapPlayer(int nStageNo)
+{
+	//ステージごとの場所変更（仮）
+	if (nStageNo == 1)
+	{
+		g_MapPlayerpos = D3DXVECTOR3(1063.0f, 140.0f, 0.0f);
+	}
+	else if (nStageNo == 2)
+	{
+		g_MapPlayerpos = D3DXVECTOR3(1153.0f, 140.0f, 0.0f);
+	}
+	else if (nStageNo == 3)
+	{
+		g_MapPlayerpos = D3DXVECTOR3(1153.0f, 55.0f, 0.0f);
+	}
+	else if (nStageNo == 4)
+	{
+		g_MapPlayerpos = D3DXVECTOR3(1063.0f, 55.0f, 0.0f);
+	}
+
+
+
+
+	//頂点バッファの生成・頂点情報の設定
+	VERTEX_2D* pVtx;
+	//頂点バッファをロックし、頂点情報へのポインタを取得
+	g_pVtxBuffMapPlayer->Lock(0, 0, (void**)&pVtx, 0);
+
+	//頂点座標の設定
+	pVtx[0].pos = D3DXVECTOR3(g_MapPlayerpos.x, g_MapPlayerpos.y, 0.0f);
+	pVtx[1].pos = D3DXVECTOR3(g_MapPlayerpos.x + MAPSIZE_PLAYER, g_MapPlayerpos.y, 0.0f);
+	pVtx[2].pos = D3DXVECTOR3(g_MapPlayerpos.x, g_MapPlayerpos.y + MAPSIZE_PLAYER, 0.0f);
+	pVtx[3].pos = D3DXVECTOR3(g_MapPlayerpos.x + MAPSIZE_PLAYER, g_MapPlayerpos.y + MAPSIZE_PLAYER, 0.0f);
+
+	//頂点バッファをアンロック
+	g_pVtxBuffMapPlayer->Unlock();
+}
