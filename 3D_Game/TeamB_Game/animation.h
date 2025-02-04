63152f4a9fc7f84028f1b11eee79b4adbcdba5a7
Kensaku-Hatori <241316@st.yoshida-g.ac.jp>
2025-02-04 10:40:01 +0900

    コメント

--- 3D_Game/TeamB_Game/animation.h
+++ 3D_Game/TeamB_Game/animation.h
@@ -1,11 +1,16 @@
+//***************************************
+//
+/// モーションを再生する処理[animation.h]
+// Hatori Kensaku
+//
+//***************************************
 #ifndef ANIMATION_H_
 #define ANIMATION_H_
 
 #include "main.h"
 #include "key.h"
 
-void UpdateMotion(OBJECTINFO *Motion);
-void SetMotion(MOTIONTYPE MotionType, OBJECTINFO*Motion);
-void MotionBlend(void);
+void UpdateMotion(OBJECTINFO *Motion);						// モーション再生処理
+void SetMotion(MOTIONTYPE MotionType, OBJECTINFO*Motion);	// モーション設定処理
 
 #endif // !ANIMATION_H_
