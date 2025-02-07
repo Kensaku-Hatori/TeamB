1a1c7471df1cc3135c17c12fb9bcbb73a0739d5f
大野莉緒 <241302@st.yoshida-g.ac.jp>
2025-02-07 10:03:02 +0900

    保存

---
+++ 3D_Game/TeamB_Game/guide.h
@@ -0,0 +1,20 @@
+//=======================================================
+// 
+// 誘導する[guide.h]
+// Author:Rio Ohno
+// 
+//=======================================================
+
+#ifndef GUIDE_H_
+#define GUIDE_H_
+
+#include "main.h"
+
+//プロトタイプ宣言
+void InitGuide();
+void UninitGuide();
+void UpdateGuide();
+void DrawGuide();
+void SetGuide();
+
+#endif // !GUIDE_H_
