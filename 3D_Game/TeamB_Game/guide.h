4d0f27afa9846f2b669f1e662627257fb1bd2344
大野莉緒 <241302@st.yoshida-g.ac.jp>
2025-02-07 14:33:54 +0900

    a

--- 3D_Game/TeamB_Game/guide.h
+++ 3D_Game/TeamB_Game/guide.h
@@ -15,16 +15,6 @@
 #define GUIDE_HEIGHT (float)(40.0f)
 #define GUIDE_RADIUS (float)(25.0f)
 
-//circle
-typedef struct
-{
-	D3DXVECTOR3 pos;
-	D3DXVECTOR3 rot;
-	D3DXMATRIX mtxWorld;
-	LPDIRECT3DVERTEXBUFFER9 VtxBuff;
-	LPDIRECT3DINDEXBUFFER9 IndxBuff;
-}Circle;
-
 //arrow
 typedef struct
 {
