9fcb6c00da0f752bb3a8e8312e84670e6433e220
Kensaku-Hatori <241316@st.yoshida-g.ac.jp>
2025-02-14 13:29:09 +0900

    当たり判定

---
+++ 3D_Game/TeamB_Game/collision.h
@@ -0,0 +1,23 @@
+//*************************************
+//
+// 当たり判定をまとめたh[collision.h]
+// Author Kensaku Hatori
+//
+//*************************************
+#ifndef COLLISION_H_
+#define COLLISION_H_
+#include "main.h"
+#include "model.h"
+
+// 立方体の当たり判定に必要な構造体
+typedef struct
+{
+	D3DXVECTOR3 FacePos[4];	// 面の中心ポス
+	D3DXVECTOR3 Nor[4];		// 面の法線
+}BoxCollision;
+
+bool collisionboxtopoint(D3DXVECTOR3 Pos,BoxCollision BoxInfo);							// 点と矩形の当たり判定
+bool collisioncircle(D3DXVECTOR3 Pos, float fRadius, D3DXVECTOR3 Pos1, float fRadius1);	// 球と球の当たり判定
+bool collisionobb(OBB obb,OBB obb1,D3DXVECTOR3 Pos,D3DXVECTOR3 Pos1);					// OBBとOBBの当たり判定
+void PushPosition(D3DXVECTOR3 *Pos,D3DXVECTOR3 VecMove,D3DXVECTOR3 Nor);				// 壁刷り関数
+#endif // !COLLISION_H_
