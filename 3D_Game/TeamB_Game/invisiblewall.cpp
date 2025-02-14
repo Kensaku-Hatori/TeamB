eea8c3e5acba4dea3e4853f10a88754608a2f1a6
Kensaku-Hatori <241316@st.yoshida-g.ac.jp>
2025-02-14 10:00:51 +0900

    みえないかべ　

--- 3D_Game/TeamB_Game/invisiblewall.cpp
+++ 3D_Game/TeamB_Game/invisiblewall.cpp
@@ -6,6 +6,7 @@
 //******************************************
 
 #include "invisiblewall.h"
+#include "enemy.h"
 
 Invisiblewall g_InvisibleWall[WALLTYPE_MAX];
 
@@ -178,8 +179,6 @@ void CollisionInvisibleWall(Invisiblewall Box, D3DXVECTOR3 Point , D3DXVECTOR3 &
 	fDotRight = D3DXVec3Dot(&RightVec, &Box.norRight);
 	fDotBottom = D3DXVec3Dot(&BottomVec, &Box.norBottom);
 	fDotWall = D3DXVec3Dot(&WallVec, &Box.norWall);
-	test.x = fDotLeft;
-	test.y = fDotRight;
 
 	// 各面とプレイヤーとの内積結果が０以上で出口に対して水平な面の内積結果が０以上だったら
 	if (fDotTop < 0 && fDotLeft < 0 && fDotRight < 0 && fDotBottom <= 0 && fDotWall < 0)
